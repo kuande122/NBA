dbd27356a25e9586568c9b2ce8d08baa489f5662
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-14 12:46:09 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,5 +5,5 @@ teams_list = {'Boston Celtics', 'Brooklyn Nets', 'New York Knicks', 'Philadelphi
               'Golden State Warriors', 'Los Angeles Clippers', 'Los Angeles Lakers', 'Phoenix Suns','Sacramento Kings','Dallas Mavericks', 'Houston Rockets', 'Memphis Grizzlies', 'New Orleans Pelicans',
               'San Antonio Spurs','Atlanta Hawks', 'Charlotte Hornets', 'Miami Heat', 'Orlando Magic','Washington Wizards','Denver Nuggets', 'Minnesota Timberwolves', 'Oklahoma City Thunder', 
               'Portland Trail Blazers','Utah Jazz'}
-st.sidebar.header('選擇球隊')
+st.sidebar.header('請選擇球隊及想查看數據')
 option = st.sidebar.selectbox('選擇球隊？',teams_list)
