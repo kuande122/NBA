bf135364237b36bb73fafcb08d2b21c353893dd1
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-17 22:37:28 +0800

    Update teams_information.py

--- teams_information.py
+++ teams_information.py
@@ -181,7 +181,7 @@ def teams_information(option):
     col1, col2= st.columns(2)
     col1.metric("聯盟冠軍🏆", "0  次")
     col2.metric("分組冠軍🏆", "0  次")   
-  if option=="OklahomCity Thunder":
+  if option=="Oklahom City Thunder":
     col1, col2 = st.columns(2)
     with col1:
       image = Image.open('teams logo/Oklahoma City Thunder.png')
