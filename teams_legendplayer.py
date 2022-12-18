9c792541f65e86e2227a65dd7fdd8ce33b48f1e6
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-18 22:23:15 +0800

    Update teams_legendplayer.py

--- teams_legendplayer.py
+++ teams_legendplayer.py
@@ -3,3 +3,7 @@ from PIL import Image
 import pandas as pd
 import xlrd  
 import openpyxl
+def teams_legendplayer():
+  if teams_list=='Boston Celtics':
+    legendplayer_list={'Bill Russell','Larry Bird','Paul Pierce'}
+  return legendplayer_list
