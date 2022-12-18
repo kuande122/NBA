1b92810cd17932610847f82710df282a39d1676a
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-18 23:38:24 +0800

    Update teams_legendplayer.py

--- teams_legendplayer.py
+++ teams_legendplayer.py
@@ -6,5 +6,5 @@ import openpyxl
 def teams_legendplayer():
   legend_list={'Boston Celtics':{'Bill Russell','Larry Bird','Paul Pierce'},'Brooklyn Nets':{'Julius Erving','Jason Kidd','Derrick Coleman'}}
   option_legendplayer = st.sidebar.selectbox('選擇球員？',legend_list[option])
-return option_legendplayer
+  return option_legendplayer
 option_legendplayer=teams_legendplayer()
