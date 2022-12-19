6d457f953687b9b0be287ca31a97752c2e14d3b4
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 03:11:09 +0800

    Update analysis_chart.py

--- analysis_chart.py
+++ analysis_chart.py
@@ -49,7 +49,8 @@ def analysis_chart(option_teams):
             st.pyplot(plt)
         
         if option_data=='進攻籃板': #長條圖
-           teams_data.plot.bar(x='年度',y='進攻籃板')
-           league_data.plot.bar(x='年度',y='進攻籃板')
+           pit.plot.bar(teams_data.年度,teams_data.進攻籃板)
+           #teams_data.plot.bar(x='年度',y='進攻籃板')
+           #league_data.plot.bar(x='年度',y='進攻籃板')
            st.pyplot(plt)
-           st.pyplot(plt)
+           
