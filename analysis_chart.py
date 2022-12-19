dba1b5e6bb83b0b91af00a26ff57e7a34a492507
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 02:44:54 +0800

    Update analysis_chart.py

--- analysis_chart.py
+++ analysis_chart.py
@@ -18,5 +18,5 @@ def analysis_chart(option_teams):
             plt.plot(league_data.年度,league_data.三分球命中率,'.-' )
         plt.xlabel('Season',fontsize="10")
         plt.title(option_teams+" "+data_list[option_data]+" vs League Average")
-        plt.legend(labels=[option_teams+" "+data_list[option_data],"League Average"+data_list[option_data]], loc = 'best')
+        plt.legend(labels=[option_teams+" "+data_list[option_data],"League Average "+data_list[option_data]], loc = 'best')
         st.pyplot(plt) 
