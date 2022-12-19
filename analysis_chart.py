f886770b3ab5d66163fb9f2afd1df82dde873874
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 02:14:00 +0800

    Update analysis_chart.py

--- analysis_chart.py
+++ analysis_chart.py
@@ -7,18 +7,16 @@ import matplotlib.pyplot as plt #matplotlib(資料繪圖)
 def analysis_chart():
     data_list={"三分球命中率":{"3P%"},"兩分球命中率":{"2P%"},"罰球命中率":{"FT%"},"投籃命中率":{"FG%"},"進攻籃板":{"ORB"},"防守籃板":{"DRB"},
                "總籃板":{"TRB"},"助攻":{"AST"},"抄截":{"STL"},"火鍋":{"BLK"},"失誤":{"TOV"},"犯規":{"PF"},"得分":{"PTS"}}
-    option_data = st.selectbox('想查看數據？',data_list)
-#teams_data=pd.read_excel("nbateamsdata.xlsx",sheet_name=option) 
-#area_data=pd.read_excel("nbateamsdata.xlsx",sheet_name=area_list[option_area]) 
-#league_data=pd.read_excel("nbateamsdata.xlsx",sheet_name='League Average') 
-#if option1=='三分球命中率':
-  #col1,col2=st.columns(2)
-  #with col1:
-  #st.dataframe(area_data)         
-  #with col2:
-   #plt.style.use("ggplot")
-   #plt.plot(teams_data.年度,teams_data.三分球命中率 ,'.-' ) 
-   #plt.plot(league_data.年度,league_data.三分球命中率,'.-' )
-   #plt.xlabel('Season',fontsize="10")
-   #plt.title(option+data_list[option1]+'vs League AVG')
-   #st.pyplot(plt) 
+    teams_data=pd.read_excel("nbateamsdata.xlsx",sheet_name=option_teams) 
+    league_data=pd.read_excel("nbateamsdata.xlsx",sheet_name='League Average') 
+    col1,col2=st.columns((6,4))
+    with col1:
+        option_data = st.selectbox('想查看數據？',data_list)
+    with col2:
+        if option1=='三分球命中率':
+            plt.style.use("ggplot")
+            plt.plot(teams_data.年度,teams_data.三分球命中率 ,'.-' ) 
+            plt.plot(league_data.年度,league_data.三分球命中率,'.-' )
+            plt.xlabel('Season',fontsize="10")
+    plt.title(option+data_list[option1]+'vs League AVG')
+    st.pyplot(plt) 
