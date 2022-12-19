e2839fdbf51937dbb90b2930f1cc9b944ea6c3e2
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 02:56:07 +0800

    Update analysis_chart.py

--- analysis_chart.py
+++ analysis_chart.py
@@ -12,29 +12,40 @@ def analysis_chart(option_teams):
     with col1:
         option_data = st.selectbox('想查看數據？',data_list)
     with col2:
-      if option_data=='三分球命中率'or'兩分球命中率'or'罰球命中率'or'投籃命中率':
-        if option_data=='三分球命中率':
+        if option_data=='三分球命中率': #折線圖
             plt.style.use("ggplot")
             plt.plot(teams_data.年度,teams_data.三分球命中率 ,'.-' ) 
             plt.plot(league_data.年度,league_data.三分球命中率,'.-' )
+            plt.xlabel('Season',fontsize="10")
+            plt.title(option_teams+" "+data_list[option_data]+" vs League Average")
+            plt.legend(labels=[option_teams+" "+data_list[option_data],"League Average "+data_list[option_data]], loc = 'best')
+            st.pyplot(plt)
             
         if option_data=='兩分球命中率':
             plt.style.use("ggplot")
             plt.plot(teams_data.年度,teams_data.兩分球命中率 ,'.-' ) 
             plt.plot(league_data.年度,league_data.兩分球命中率,'.-' )
-        
+            plt.xlabel('Season',fontsize="10")
+            plt.title(option_teams+" "+data_list[option_data]+" vs League Average")
+            plt.legend(labels=[option_teams+" "+data_list[option_data],"League Average "+data_list[option_data]], loc = 'best')
+            st.pyplot(plt)
+            
         if option_data=='罰球命中率':
             plt.style.use("ggplot")
             plt.plot(teams_data.年度,teams_data.罰球命中率 ,'.-' ) 
             plt.plot(league_data.年度,league_data.罰球命中率,'.-' )
- 
+            plt.xlabel('Season',fontsize="10")
+            plt.title(option_teams+" "+data_list[option_data]+" vs League Average")
+            plt.legend(labels=[option_teams+" "+data_list[option_data],"League Average "+data_list[option_data]], loc = 'best')
+            st.pyplot(plt)
+            
         if option_data=='投籃命中率':
             plt.style.use("ggplot")
             plt.plot(teams_data.年度,teams_data.投籃命中率 ,'.-' ) 
             plt.plot(league_data.年度,league_data.投籃命中率,'.-' )
-        plt.xlabel('Season',fontsize="10")
-        plt.title(option_teams+" "+data_list[option_data]+" vs League Average")
-        plt.legend(labels=[option_teams+" "+data_list[option_data],"League Average "+data_list[option_data]], loc = 'best')
-        st.pyplot(plt)
-      if option_data=='進攻籃板'or'防守籃板'or'總籃板'or'助攻'or"抄截"or"火鍋"or"失誤"or"犯規"or"得分":
-        st.write('s')
+            plt.xlabel('Season',fontsize="10")
+            plt.title(option_teams+" "+data_list[option_data]+" vs League Average")
+            plt.legend(labels=[option_teams+" "+data_list[option_data],"League Average "+data_list[option_data]], loc = 'best')
+            st.pyplot(plt)
+        
+        if option_data=='進攻籃板': #長條圖
