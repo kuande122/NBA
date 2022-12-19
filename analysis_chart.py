b95613819c0655548c8fcd3d5c355381c280d9eb
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 02:10:53 +0800

    Update analysis_chart.py

--- analysis_chart.py
+++ analysis_chart.py
@@ -4,9 +4,9 @@ import openpyxl         #excel套件
 import pandas as pd     #pandas套件(資料分析)
 import matplotlib.pyplot as plt #matplotlib(資料繪圖)
 
-#def analysis_chart():
-    data_list={"三分球命中率":"3P%","兩分球命中率":"2P%","罰球命中率":"FT%","投籃命中率":"FG%","進攻籃板":'ORB',"防守籃板":"DRB",
-               "總籃板":"TRB","助攻":"AST","抄截":"STL","火鍋":"BLK","失誤":"TOV","犯規":"PF","得分":"PTS"}
+def analysis_chart():
+    data_list={"三分球命中率":{"3P%"},"兩分球命中率":{"2P%"},"罰球命中率":{"FT%"},"投籃命中率":{"FG%"},"進攻籃板":{"ORB"},"防守籃板":{"DRB"},
+               "總籃板":{"TRB"},"助攻":{"AST"},"抄截":{"STL"},"火鍋":{"BLK"},"失誤":{"TOV"},"犯規":{"PF"},"得分":{"PTS"}}
     option_data = st.selectbox('想查看數據？',data_list)
 #teams_data=pd.read_excel("nbateamsdata.xlsx",sheet_name=option) 
 #area_data=pd.read_excel("nbateamsdata.xlsx",sheet_name=area_list[option_area]) 
