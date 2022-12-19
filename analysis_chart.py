53e8ae4511265aa314868efbbeeb81f98a8728df
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 03:21:31 +0800

    Update analysis_chart.py

--- analysis_chart.py
+++ analysis_chart.py
@@ -5,7 +5,7 @@ import pandas as pd     #pandas套件(資料分析)
 import matplotlib.pyplot as plt #matplotlib(資料繪圖)
 def analysis_chart(option_teams):
     data_list={"三分球命中率":"3P%","兩分球命中率":"2P%","罰球命中率":"FT%","投籃命中率":"FG%","進攻籃板":"ORB","防守籃板":"DRB",
-               "總籃板":"TRB","助攻":"AST","抄截":"STL","火鍋":"BLK","失誤":"TOV","犯規":"PF","得分":"PTS"}
+               "總籃數數":"TRB","助攻":"AST","抄截":"STL","火鍋":"BLK","失誤":"TOV","犯規":"PF","得分":"PTS"}
     teams_data=pd.read_excel("data/nbateamsdata.xlsx",sheet_name=option_teams) 
     league_data=pd.read_excel("data/nbateamsdata.xlsx",sheet_name="League Average") 
     col1,col2=st.columns((4,6))
@@ -41,9 +41,9 @@ def analysis_chart(option_teams):
            plt.bar(teams_data.年度,teams_data.防守籃板)
            plt.bar(league_data.年度,league_data.防守籃板)   
      
-        if option_data=='總籃板': 
-           plt.bar(teams_data.年度,teams_data.總籃板)
-           plt.bar(league_data.年度,league_data.總籃板)   
+        if option_data=='總籃板數': 
+           plt.bar(teams_data.年度,teams_data.總籃板數)
+           plt.bar(league_data.年度,league_data.總籃板數)   
        
         if option_data=='助攻': 
            plt.bar(teams_data.年度,teams_data.助攻)
