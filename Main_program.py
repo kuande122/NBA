bae708f2b6c75c3d427e7198c290e280a9d3dd65
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 01:20:21 +0800

    Update Main_program.py

--- Main_program.py
+++ Main_program.py
@@ -1,1 +1,7 @@
+import streamlit as st  
 
+#-----Set up-----------------------------------------------------
+st.set_page_config(page_title="NBA Dashboard",
+                   page_icon='🏀',
+                   layout="wide")
+st.title('NBA資訊面板系統')
