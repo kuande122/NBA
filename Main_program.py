6211c7eebe27374cdb202d84ebd7896c58c461bf
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 01:41:39 +0800

    Update Main_program.py

--- Main_program.py
+++ Main_program.py
@@ -1,6 +1,7 @@
 import streamlit as st  #streamlit套件
 import xlrd             #excel套件
 import openpyxl         #excel套件
+import pandas as pd     #pandas套件(資料分析)
 import teams_information
 import teams_map
 
