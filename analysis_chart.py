1dcfe817e17c524412c5fb8f378a0069a1268639
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 02:38:00 +0800

    Update analysis_chart.py

--- analysis_chart.py
+++ analysis_chart.py
@@ -4,8 +4,8 @@ import openpyxl         #excel套件
 import pandas as pd     #pandas套件(資料分析)
 import matplotlib.pyplot as plt #matplotlib(資料繪圖)
 def analysis_chart(option_teams):
-    data_list={"三分球命中率":["3P%"],"兩分球命中率":["2P%"],"罰球命中率":["FT%"],"投籃命中率":["FG%"],"進攻籃板":["ORB"],"防守籃板":["DRB"],
-               "總籃板":["TRB"],"助攻":["AST"],"抄截":["STL"],"火鍋":["BLK"],"失誤":["TOV"],"犯規":["PF"],"得分":["PTS"]}
+    data_list={"三分球命中率":"3P%","兩分球命中率":"2P%","罰球命中率":"FT%","投籃命中率":"FG%","進攻籃板":"ORB","防守籃板":"DRB",
+               "總籃板":"TRB","助攻":"AST","抄截":"STL","火鍋":"BLK","失誤":"TOV","犯規":"PF","得分":"PTS"}
     teams_data=pd.read_excel("data/nbateamsdata.xlsx",sheet_name=option_teams) 
     league_data=pd.read_excel("data/nbateamsdata.xlsx",sheet_name="League Average") 
     col1,col2=st.columns((4,6))
