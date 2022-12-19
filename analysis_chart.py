e530978373fb1dd5975385145d38db5e68526961
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 03:20:17 +0800

    Update analysis_chart.py

--- analysis_chart.py
+++ analysis_chart.py
@@ -12,33 +12,63 @@ def analysis_chart(option_teams):
     with col1:
         option_data = st.selectbox('想查看數據？',data_list)
     with col2:
-        if option_data=='三分球命中率': #折線圖
+#-----折線圖---------------------------------------------------------------
+        if option_data=='三分球命中率':
             plt.style.use("ggplot")
             plt.plot(teams_data.年度,teams_data.三分球命中率 ,'.-' ) 
             plt.plot(league_data.年度,league_data.三分球命中率,'.-' )
-         
-            
+             
         if option_data=='兩分球命中率':
             plt.style.use("ggplot")
             plt.plot(teams_data.年度,teams_data.兩分球命中率 ,'.-' ) 
-            plt.plot(league_data.年度,league_data.兩分球命中率,'.-' )
-        
+            plt.plot(league_data.年度,league_data.兩分球命中率,'.-' )        
             
         if option_data=='罰球命中率':
             plt.style.use("ggplot")
             plt.plot(teams_data.年度,teams_data.罰球命中率 ,'.-' ) 
             plt.plot(league_data.年度,league_data.罰球命中率,'.-' )
-        
-            
+                  
         if option_data=='投籃命中率':
             plt.style.use("ggplot")
             plt.plot(teams_data.年度,teams_data.投籃命中率 ,'.-' ) 
             plt.plot(league_data.年度,league_data.投籃命中率,'.-' )
-      
-        
-        if option_data=='進攻籃板': #長條圖
+  #-----長條圖---------------------------------------------------------------            
+        if option_data=='進攻籃板':
            plt.bar(teams_data.年度,teams_data.進攻籃板)
            plt.bar(league_data.年度,league_data.進攻籃板)
+            
+        if option_data=='防守籃板': 
+           plt.bar(teams_data.年度,teams_data.防守籃板)
+           plt.bar(league_data.年度,league_data.防守籃板)   
+     
+        if option_data=='總籃板': 
+           plt.bar(teams_data.年度,teams_data.總籃板)
+           plt.bar(league_data.年度,league_data.總籃板)   
+       
+        if option_data=='助攻': 
+           plt.bar(teams_data.年度,teams_data.助攻)
+           plt.bar(league_data.年度,league_data.助攻)  
+        
+        if option_data=='抄截': 
+           plt.bar(teams_data.年度,teams_data.抄截)
+           plt.bar(league_data.年度,league_data.抄截)  
+            
+        if option_data=='火鍋': 
+           plt.bar(teams_data.年度,teams_data.火鍋)
+           plt.bar(league_data.年度,league_data.火鍋)       
+        
+        if option_data=='失誤': 
+           plt.bar(teams_data.年度,teams_data.失誤)
+           plt.bar(league_data.年度,league_data.失誤)    
+         
+        if option_data=='犯規': 
+           plt.bar(teams_data.年度,teams_data.犯規)
+           plt.bar(league_data.年度,league_data.犯規)    
+        
+        if option_data=='得分':
+           plt.bar(teams_data.年度,teams_data.得分)
+           plt.bar(league_data.年度,league_data.得分)  
+
         plt.xlabel('Season',fontsize="10")
         plt.title(option_teams+" "+data_list[option_data]+" vs League Average")
         plt.legend(labels=[option_teams+" "+data_list[option_data],"League Average "+data_list[option_data]], loc = 'best')
