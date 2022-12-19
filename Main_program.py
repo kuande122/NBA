06ceea4be0d612bcd7f78a1249c65de2acc793b0
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 01:23:55 +0800

    Update Main_program.py

--- Main_program.py
+++ Main_program.py
@@ -5,3 +5,12 @@ st.set_page_config(page_title="NBA Dashboard",
                    page_icon='🏀',
                    layout="wide")
 st.title('NBA資訊面板系統')
+#-----Sidebar----------------------------------------------------
+st.sidebar.title('請選擇區域及球隊')
+area_list={'Atlantic':['Boston Celtics', 'Brooklyn Nets', 'New York Knicks', 'Philadelphia 76ers','Toronto Raptors'],
+           'Central':['Chicago Bulls', 'Cleveland Cavaliers', 'Detroit Pistons', 'Indiana Pacers','Milwaukee Bucks'],
+           'Southeast':['Atlanta Hawks', 'Charlotte Hornets', 'Miami Heat', 'Orlando Magic','Washington Wizards'],
+           'Northwest':['Denver Nuggets', 'Minnesota Timberwolves', 'Oklahoma City Thunder','Portland Trail Blazers','Utah Jazz'],
+           'Pacific':['Golden State Warriors', 'Los Angeles Clippers', 'Los Angeles Lakers', 'Phoenix Suns','Sacramento Kings'],
+           'Southwest':['Dallas Mavericks', 'Houston Rockets', 'Memphis Grizzlies', 'New Orleans Pelicans','San Antonio Spurs']}
+option_area = st.sidebar.selectbox('選擇區域？',area_list)
