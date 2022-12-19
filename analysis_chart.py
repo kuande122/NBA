fa2efb82d58a3fa6e6e89ea8cab31a66724f01ed
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 02:06:58 +0800

    Update analysis_chart.py

--- analysis_chart.py
+++ analysis_chart.py
@@ -4,7 +4,7 @@ import openpyxl         #excel套件
 import pandas as pd     #pandas套件(資料分析)
 import matplotlib.pyplot as plt #matplotlib(資料繪圖)
 
-def analysis_chart():
+#def analysis_chart():
     data_list={"三分球命中率":"3P%","兩分球命中率":"2P%","罰球命中率":"FT%","投籃命中率":"FG%","進攻籃板":'ORB',"防守籃板":"DRB",
                "總籃板":"TRB","助攻":"AST","抄截":"STL","火鍋":"BLK","失誤":"TOV","犯規":"PF","得分":"PTS"}
     option_data = st.selectbox('想查看數據？',data_list)
