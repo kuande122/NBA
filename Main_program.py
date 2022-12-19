52bd938c80d9a753a0e0525ee428f70bf8a319bc
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 01:35:45 +0800

    Update Main_program.py

--- Main_program.py
+++ Main_program.py
@@ -25,3 +25,25 @@ option_teams = st.sidebar.selectbox('選擇球隊？',area_list[option_area])
 #-----teams_information&teams_map-----------------------------------------
 teams_information.teams_information(option_teams)
 teams_map.teams_map(option_teams)
+
+
+#-----球隊戰績-------------------------------------------------------------
+col1,col2=st.columns((6,4))
+with col1:
+  st.markdown('### 球隊戰績')
+  teams_data=pd.read_excel("nbateamsdata.xlsx",sheet_name=option)
+  st.dataframe(teams_data)
+with col2: 
+  dount_chart_df = pd.read_excel("nbateamsdata(dount_chart).xlsx",sheet_name=option)
+  st.markdown('### 年度戰績Donut chart')
+  donut_theta = st.selectbox('選擇年度', ('14-15年', '15-16年','16-17年','17-18年','18-19年','19-20年','20-21年','21-22年'))
+  plost.donut_chart(
+              data=dount_chart_df ,
+              theta=donut_theta,
+              color='項目',
+              legend='bottom',
+              use_container_width=True)
+st.markdown('### 2021-22球員戰績')
+players_data=pd.read_excel("21-22playersdata.xlsx",sheet_name=option)
+st.dataframe(players_data)
+
