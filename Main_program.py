9ebaa3ffdd0a04e337aaefe276a58017a852331e
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-22 00:03:57 +0800

    Update Main_program.py

--- Main_program.py
+++ Main_program.py
@@ -98,7 +98,7 @@ st.markdown('### TOP 10 RANK👑')
 rank_data = pd.read_excel("data/Rank.xlsx",sheet_name=option_teams,usecols='B,C')
 rank_data = rank_data[0:10]
 plt.bar(rank_data.PLAYER,rank_data.Games)
-st.pypolt(plt)
+st.pyplot(plt)
 
 
 
