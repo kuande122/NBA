0943c69a895417f6e3913f852737b2ef0fede246
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-22 00:49:48 +0800

    Update analysis_chart.py

--- analysis_chart.py
+++ analysis_chart.py
@@ -34,9 +34,8 @@ def analysis_chart(option_teams):
             plt.plot(league_data.年度,league_data.投籃命中率,'.-' )
 #-----長條圖---------------------------------------------------------------            
         if option_data=='進攻籃板':
-           width = 0.3
-           plt.bar(teams_data.年度,teams_data.進攻籃板)
-           plt.bar(league_data.年度 + width,league_data.進攻籃板, width)
+           plt.bar(teams_data.年度,teams_data.進攻籃板,barmode='group')
+           plt.bar(league_data.年度,league_data.進攻籃板, barmode='group')
             
         if option_data=='防守籃板': 
            plt.bar(teams_data.年度,teams_data.防守籃板)
