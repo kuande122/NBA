e6087481a8da1aa373de18a7cb192c6b523428b4
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-22 23:18:26 +0800

    Update analysis_chart.py

--- analysis_chart.py
+++ analysis_chart.py
@@ -104,7 +104,7 @@ def TOP10RANK(option_teams):
      ax.set_title(option_teams+' TOP 10 Rank to Field Goals')
      ax.set( xlabel="Field Goals",ylabel='PLAYER')
      st.pyplot(fig)
-    
+    x=st.button('點取看更多')
     if x:
         col4,col5,col6=st.columns(3)
         with col4:  
