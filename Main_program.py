5dff6a5d3e76611975c62e1442e0f479163fa791
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-22 22:46:24 +0800

    Update Main_program.py

--- Main_program.py
+++ Main_program.py
@@ -132,7 +132,7 @@ with col4:
   st.pyplot(fig)
 
 with col5:
-  rank_data.sort_values(by='FreeThrows,',inplace=True,ascending=False)
+  rank_data.sort_values(by='FreeThrows',inplace=True,ascending=False)
   fig, ax = plt.subplots()
   ax = sns.barplot(x=rank_data.FreeThrows, y=rank_data.PLAYER4)
   ax.set_title(option_teams+' TOP 10 Rank to FreeThrows')
@@ -149,7 +149,7 @@ with col6:
 
 col7,col8,col9=st.columns(3)
 with col7:
-  rank_data.sort_values(by='Assists,',inplace=True,ascending=False)
+  rank_data.sort_values(by='Assists',inplace=True,ascending=False)
   fig, ax = plt.subplots()
   ax = sns.barplot(x=rank_data.Assists, y=rank_data.PLAYER6)
   ax.set_title(option_teams+' TOP 10 Rank to Assists')
