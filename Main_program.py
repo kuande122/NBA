285386d56a7fb1d921179c09982e772214ddd9f9
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-22 22:38:11 +0800

    Update Main_program.py

--- Main_program.py
+++ Main_program.py
@@ -99,7 +99,7 @@ st.markdown('### TOP 10 RANK👑')
 col1,col2,col3=st.columns(3)
 
 with col1:
-  rank_data = pd.read_excel("data/Rank.xlsx",sheet_name=option_teams)
+  rank_data = pd.read_excel("data/Rank.xlsx",sheet_name=option_teams,usecols=["PLAYER", "Games"])
   #rank_data.sort_values(by='Games',inplace=True,ascending=False)
   fig, ax = plt.subplots()
   ax = sns.barplot(x=rank_data.Games, y=rank_data.PLAYER)
