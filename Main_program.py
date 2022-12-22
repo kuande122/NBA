d429b0fc503cefc8e455996cb0cd16cd5c878209
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-22 22:35:22 +0800

    Update Main_program.py

--- Main_program.py
+++ Main_program.py
@@ -100,7 +100,7 @@ col1,col2,col3=st.columns(3)
 
 with col1:
   rank_data = pd.read_excel("data/Rank.xlsx",sheet_name=option_teams)
-  rank_data.sort_values(by='Games',inplace=True,ascending=False)
+  #rank_data.sort_values(by='Games',inplace=True,ascending=False)
   fig, ax = plt.subplots()
   ax = sns.barplot(x=rank_data.Games, y=rank_data.PLAYER)
   ax.set_title(option_teams+' TOP 10 Rank to Games')
